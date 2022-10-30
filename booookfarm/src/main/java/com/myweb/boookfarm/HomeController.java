6a2d6710bf54edcb6a32734cd04a341751a67617
kang <kang@host.docker.internal>
2022-10-30 16:55:31 +0900

    프론트 수정

--- booookfarm/src/main/java/com/myweb/boookfarm/HomeController.java
+++ booookfarm/src/main/java/com/myweb/boookfarm/HomeController.java
@@ -8,8 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import oracle.jdbc.proxy.annotation.Post;
 
 /**
  * Handles requests for the application home page.
@@ -25,6 +30,16 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		return "standard/standard";
+	}
+	
+	@GetMapping(value = "/list")
+	@ResponseBody
+	public String bookList() {
+		//등록된 모든 책 리스트
+			
+		
+		
+		return"";
 	}
 	
 	@RequestMapping(value = "/detail", method = RequestMethod.GET)
