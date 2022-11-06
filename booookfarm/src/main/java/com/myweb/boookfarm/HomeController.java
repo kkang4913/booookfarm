33acaf3858f833ec5fb15486710e40f672a6ecfb
kang <kang@host.docker.internal>
2022-11-06 20:12:05 +0900

    북 리스트 오류 수정, 페이징 기능 수정, 페이지 필터 변경

--- booookfarm/src/main/java/com/myweb/boookfarm/HomeController.java
+++ booookfarm/src/main/java/com/myweb/boookfarm/HomeController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.myweb.boookfarm.detail.model.BookDetailDTO;
+import com.myweb.boookfarm.member.model.PagingDTO;
 import com.myweb.boookfarm.model.BookDTO;
 import com.myweb.boookfarm.service.BookfarmService;
 
@@ -39,35 +40,38 @@ public class HomeController {
 
 	@GetMapping(value = "/list",produces="application/json; charset=utf-8")
 	@ResponseBody
-	public String bookList() {
+	public String bookList(@RequestParam(defaultValue = "1", required = false) int page //현재페이지
+			,@RequestParam("page_count")int pagecount) {
 		//등록된 모든 책 리스트
-		List<BookDTO> datas = service.getData();
-		int pageCnt = service.getDataCnt();
-		
-		JSONArray data_arr = new JSONArray();
-		JSONObject rtn_data = new JSONObject();
-		JSONObject page_arr = new JSONObject();
-		for(BookDTO bookList : (List<BookDTO>) datas) {
+		Map res_data = service.getAll(page, pagecount);
+		List datas = (List) res_data.get("datas"); // 가져온 데이터 리스트
+		PagingDTO pager = (PagingDTO) res_data.get("page_data"); // 가져온 페이징 객체
+		JSONArray data_arr = new JSONArray(); // 가져온 데이터 리스트를 넣는 배열
+		JSONObject page_obj = new JSONObject(); // 가져온 페이징 데이터를 넣는 객체
+		JSONObject rtn_data = new JSONObject(); // 결과값
+		for (BookDTO bfarm : (List<BookDTO>) datas) {
 			JSONObject json = new JSONObject();
-			json.put("bookCode", bookList.getBookCode());
-			json.put("bookTitle", bookList.getBookTitle());
-			json.put("bookAuthor", bookList.getBookAuthor());
-			json.put("bookInfo", bookList.getBookInfo());
-			json.put("bookPrice", bookList.getBookPrice());
-			json.put("bookDiscount", bookList.getBookDiscount());
-			json.put("isbn", bookList.getIsbn());
-			json.put("bookCategory", bookList.getBookCategory());
-			json.put("stock", bookList.getStock());
-			json.put("publisher", bookList.getPublisher());
-			json.put("bookCondition", bookList.getBookCondition());
-			json.put("bookConditionInfo", bookList.getBookConditionInfo());
-			json.put("bookImgPath", bookList.getBookImgPath());
-			json.put("createDate", bookList.getCreateDate());
+			json.put("bookCode", bfarm.getBookCode());
+			json.put("bookTitle", bfarm.getBookTitle());
+			json.put("bookAuthor", bfarm.getBookAuthor());
+			json.put("bookPrice", bfarm.getBookPrice());
+			json.put("bookDiscount", bfarm.getBookDiscount());
+			json.put("isbn", bfarm.getIsbn());
+			json.put("bookCategory", bfarm.getBookCategory());
+			json.put("publisher", bfarm.getPublisher());
+			json.put("bookCondition", bfarm.getBookCondition());
+			json.put("bookImgPath", bfarm.getBookImgPath());
+			json.put("createDate", bfarm.getCreateDate());
 			data_arr.add(json);
 		}
-		page_arr.put("Cnt", pageCnt);
+		page_obj.put("c_page", pager.getCurrentPageNumber());
+		page_obj.put("pagelist", pager.getPageList());
+		page_obj.put("n_page", pager.getNextPageNumber());
+		page_obj.put("p_page", pager.getPrevPageNumber());
+		page_obj.put("is_npage", pager.isNextPage());
+		page_obj.put("is_ppage", pager.isPrevPage());
 		rtn_data.put("datas", data_arr);
-		rtn_data.put("pages", page_arr);
+		rtn_data.put("pager", page_obj);
 		return rtn_data.toJSONString();
 	}
 }
